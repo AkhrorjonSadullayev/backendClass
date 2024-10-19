53d26cd32a4f71d8af04de5049a7f4d71ff88335
Akhrorjon <ahrorsadullayev@gmail.com>
2024-10-19 15:41:30 +0900

    Add files via upload

---
+++ utils/secrets.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const PORT = process.env.PORT || 7070;
+export const ENVIRONMENT = process.env.ENIRONMENT || 7070;
+export const REG_KEY = process.env.REG_KEY;
